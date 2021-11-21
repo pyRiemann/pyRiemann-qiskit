e6efe84b3ac5b834dd7a9b357d9c9a18ef13a700
gcattan <gregoire.cattan@ibm.com>
2021-11-21 13:33:45 +0100

    add documentation for factory

--- pyriemann_qiskit/utils/hyper_params_factory.py
+++ pyriemann_qiskit/utils/hyper_params_factory.py
@@ -2,6 +2,22 @@ from qiskit.circuit.library import ZZFeatureMap
 
 
 def gen_zz_feature_map(reps=2, entanglement='linear'):
+    """Return a callable that generate a ZZFeatureMap.
+    A feature map encodes data into a quantum state.
+    A ZZFeatureMap is a second-order Pauli-Z evolution circuit.
+    
+    Parameters
+    ----------
+    reps : int
+        The number of repeated circuits, has a min. value of 1.
+    entanglement : Union[str, List[List[int]], Callable[[int], List[int]]]
+        Specifies the entanglement structure.
+
+    Returns
+    -------
+    ret : ZZFeatureMap
+        An instance of ZZFeatureMap
+    """
     return lambda dim: ZZFeatureMap(feature_dimension=dim,
                                     reps=reps,
                                     entanglement=entanglement)
