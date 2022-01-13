2b2cf7da213d0f66ccfda7c58e3c9118a068e0e8
gcattan <gregoire.cattan@ibm.com>
2022-01-13 20:33:50 +0100

    refactore test of gen_ZZ_Featuremap

--- tests/test_utils_hyper_params_factory.py
+++ tests/test_utils_hyper_params_factory.py
@@ -4,40 +4,42 @@ from pyriemann_qiskit.utils.hyper_params_factory import (gen_zz_feature_map,
                                                          get_spsa)
 
 
-@pytest.mark.parametrize(
-    'entanglement', ['full', 'linear', 'circular', 'sca']
-)
-def test_gen_zz_feature_map_entangl_strings(entanglement):
-    """Test gen_zz_feature_map with different string options of entanglement"""
-    n_features = 2
-    feature_map = gen_zz_feature_map(entanglement=entanglement)(n_features)
-    assert isinstance(feature_map.parameters, set)
+class TestGenZZFeatureMap:
+
+    @pytest.mark.parametrize(
+        'entanglement', ['full', 'linear', 'circular', 'sca']
+    )
+    def test_entangl_strings(self, entanglement):
+        """Test gen_zz_feature_map with different string options of entanglement"""
+        n_features = 2
+        feature_map = gen_zz_feature_map(entanglement=entanglement)(n_features)
+        assert isinstance(feature_map.parameters, set)
 
 
-def test_gen_zz_feature_map_entangl_idx(get_pauli_z_linear_entangl_idx):
-    """Test gen_zz_feature_map with valid indices value"""
-    n_features = 2
-    reps = 2
-    indices = get_pauli_z_linear_entangl_idx(reps, n_features)
-    feature_map_handle = gen_zz_feature_map(reps=reps, entanglement=indices)
-    feature_map = feature_map_handle(n_features)
-    assert isinstance(feature_map.parameters, set)
+    def test_entangl_idx(self, get_pauli_z_linear_entangl_idx):
+        """Test gen_zz_feature_map with valid indices value"""
+        n_features = 2
+        reps = 2
+        indices = get_pauli_z_linear_entangl_idx(reps, n_features)
+        feature_map_handle = gen_zz_feature_map(reps=reps, entanglement=indices)
+        feature_map = feature_map_handle(n_features)
+        assert isinstance(feature_map.parameters, set)
 
 
-def test_gen_zz_feature_map_entangl_handle(get_pauli_z_linear_entangl_handle):
-    """Test gen_zz_feature_map with a valid callable"""
-    n_features = 2
-    indices = get_pauli_z_linear_entangl_handle(n_features)
-    feature_map = gen_zz_feature_map(entanglement=indices)(n_features)
-    assert isinstance(feature_map.parameters, set)
+    def test_entangl_handle(self, get_pauli_z_linear_entangl_handle):
+        """Test gen_zz_feature_map with a valid callable"""
+        n_features = 2
+        indices = get_pauli_z_linear_entangl_handle(n_features)
+        feature_map = gen_zz_feature_map(entanglement=indices)(n_features)
+        assert isinstance(feature_map.parameters, set)
 
 
-def test_gen_zz_feature_map_entangl_invalid_value():
-    """Test gen_zz_feature_map with uncorrect value"""
-    n_features = 2
-    feature_map = gen_zz_feature_map(entanglement="invalid")(n_features)
-    with pytest.raises(ValueError):
-        feature_map.parameters
+    def test_entangl_invalid_value(self):
+        """Test gen_zz_feature_map with uncorrect value"""
+        n_features = 2
+        feature_map = gen_zz_feature_map(entanglement="invalid")(n_features)
+        with pytest.raises(ValueError):
+            feature_map.parameters
 
 
 def test_gen_two_local_default():
