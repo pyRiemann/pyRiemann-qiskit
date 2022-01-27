4ff646e0c409327537f541d2cd5086bbc5142e4e
gcattan <gregoire.cattan@ibm.com>
2022-01-27 20:50:14 +0100

    finish documentation

--- pyriemann_qiskit/utils/filtering.py
+++ pyriemann_qiskit/utils/filtering.py
@@ -6,6 +6,11 @@ class NoDimRed(TransformerMixin):
 
     A Ghost transformer that just returns the data without
     transformation.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.1
+
     """
 
     def fit(self, X, y=None):
@@ -57,12 +62,45 @@ class NaiveEvenDimRed(TransformerMixin):
 
     Reduce the dimension of the feature by two,
     keeping only even indices of the feature vector.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.1
+
     """
 
     def fit(self, X, y=None):
+        """Fit the training data.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, n_features)
+            Training vector, where `n_samples` is the number of samples and
+            `n_features` is the number of features.
+        y : ndarray, shape (n_samples, n_features)
+            Target vector relative to X.
+
+        Returns
+        -------
+        self : NaiveEvenDimRed instance
+            The NaiveEvenDimRed instance.
+        """
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X):
+        """Divide the feature dimension by two, 
+        keeping only even indices.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, n_features)
+            ndarray of feature vectors.
+
+        Returns
+        -------
+        X : ndarray, shape (n_samples, (n_features + 1) // 2)
+            The Filtered feature vectors.
+        """
         return X[:, ::2]
 
 
@@ -71,10 +109,43 @@ class NaiveOddDimRed(TransformerMixin):
 
     Reduce the dimension of the feature by two,
     keeping only odd indices of the feature vector.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.1
+
     """
 
     def fit(self, X, y=None):
+        """Fit the training data.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, n_features)
+            Training vector, where `n_samples` is the number of samples and
+            `n_features` is the number of features.
+
+        Returns
+        -------
+        self : NaiveOddDimRed instance
+            The NaiveOddDimRed instance.
+        """
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X):
+        """Divide the feature dimension by two, 
+        keeping only odd indices.
+
+        Parameters
+        ----------
+        X : ndarray, shape (n_samples, n_features)
+            ndarray of feature vectors.
+        y : ndarray, shape (n_samples,)
+            Target vector relative to X.
+
+        Returns
+        -------
+        X : ndarray, shape (n_samples, n_features // 2)
+            The Filtered feature vectors.
+        """
         return X[:, 1::2]
