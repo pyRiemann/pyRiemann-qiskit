c84da4090e96556b1a525ad063cc9f9064090133
gcattan <gregoire.cattan@ibm.com>
2022-01-27 16:35:47 +0100

    finish documentation of NoDimRed

--- pyriemann_qiskit/utils/filtering.py
+++ pyriemann_qiskit/utils/filtering.py
@@ -7,10 +7,48 @@ class NoDimRed(TransformerMixin):
     A Ghost transformer that just returns the data without
     transformation.
     """
+
     def fit(self, X, y=None):
+        """Fit the training data.
+
+        Parameters
+        ----------
+        X : object
+            Training data.
+            The type of the data does not matter,
+            as the parameter will not be used.
+        y : object
+            Target vector relative to X.
+            The type of the vector does not matter,
+            as the parameter will not be used.
+
+        Returns
+        -------
+        self : NoDimRed instance
+            The NoDimRed instance.
+        """
         return self
 
     def transform(self, X, y=None):
+        """Transform the training data.
+
+        No transformation will be applied in practice.
+        This class implements only the Ghost design pattern.
+
+        Parameters
+        ----------
+        X : object
+            Training data.
+            The type of the data does not matter.
+        y : object
+            Target vector relative to X.
+            The type of the vector does not matter.
+
+        Returns
+        -------
+        X : object
+            The same data passed through the parameter X.
+        """
         return X
 
 
@@ -20,6 +58,7 @@ class NaiveEvenDimRed(TransformerMixin):
     Reduce the dimension of the feature by two,
     keeping only even indices of the feature vector.
     """
+
     def fit(self, X, y=None):
         return self
 
@@ -33,6 +72,7 @@ class NaiveOddDimRed(TransformerMixin):
     Reduce the dimension of the feature by two,
     keeping only odd indices of the feature vector.
     """
+
     def fit(self, X, y=None):
         return self
 
