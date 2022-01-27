0ad32060ad51c20558ba0d079498f88e4cabdb92
gcattan <gregoire.cattan@ibm.com>
2022-01-27 16:26:26 +0100

    Classes' documentation

--- pyriemann_qiskit/utils/filtering.py
+++ pyriemann_qiskit/utils/filtering.py
@@ -2,6 +2,11 @@ from sklearn.base import TransformerMixin
 
 
 class NoDimRed(TransformerMixin):
+    """No dimensional reduction.
+
+    A Ghost transformer that just returns the data without
+    transformation.
+    """
     def fit(self, X, y=None):
         return self
 
@@ -9,7 +14,12 @@ class NoDimRed(TransformerMixin):
         return X
 
 
-class NaivePairDimRed(TransformerMixin):
+class NaiveEvenDimRed(TransformerMixin):
+    """Naive dimensional reduction (Even)
+
+    Reduce the dimension of the feature by two,
+    keeping only even indices of the feature vector.
+    """
     def fit(self, X, y=None):
         return self
 
@@ -17,7 +27,12 @@ class NaivePairDimRed(TransformerMixin):
         return X[:, ::2]
 
 
-class NaiveImpairDimRed(TransformerMixin):
+class NaiveOddDimRed(TransformerMixin):
+    """Naive dimensional reduction (Odd)
+
+    Reduce the dimension of the feature by two,
+    keeping only odd indices of the feature vector.
+    """
     def fit(self, X, y=None):
         return self
 
