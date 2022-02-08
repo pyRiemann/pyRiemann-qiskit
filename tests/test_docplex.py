6e17ae157b51ef17cab13f0a944543affdf4914d
Anton <toncho11@gmail.com>
2022-02-08 08:38:51 +0100

    added test

---
+++ tests/test_docplex.py
@@ -0,0 +1,9 @@
+from docplex.mp.vartype import ContinuousVarType
+from docplex.mp.model import Model
+
+def test_get_square_cont_var():
+    channels = range(4)
+    prob = Model()
+    ContinuousVarType.one_letter_symbol = lambda _: 'C'
+    return prob.continuous_var_matrix(keys1=channels, keys2=channels,
+                                      name='test', lb=-prob.infinity)
