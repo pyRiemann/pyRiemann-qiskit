01a444b5f85c9b58735fb0eb8b3db431cec8731a
gcattan <gregoire.cattan@ibm.com>
2022-02-20 20:10:31 +0100

    flake8

--- examples/ERP/compare_dim_red.py
+++ examples/ERP/compare_dim_red.py
@@ -1,12 +1,9 @@
 """
 ====================================================================
-ERP EEG decoding with Quantum Classifier.
+TODO
 ====================================================================
 
-Decoding applied to EEG data in sensor space decomposed using Xdawn.
-After spatial filtering, covariances matrices are estimated, then projected in
-the tangent space and classified with a quantum SVM classifier.
-It is compared to the classical SVM.
+TODO
 
 """
 # Author: Gregoire Cattan
@@ -14,35 +11,19 @@ It is compared to the classical SVM.
 # License: BSD (3-clause)
 
 from pyriemann_qiskit.datasets import get_mne_sample
-from pyriemann_qiskit.classification import QuantumClassifierWithDefaultRiemannianPipeline
-from pyriemann_qiskit.utils.filtering import NaivePair, NaiveImpair, NoFilter
-from pyriemann_qiskit.utils.hyper_params_factory import gen_zz_feature_map, gen_two_local, get_spsa
-from mne import io, read_events, pick_types, Epochs
-from mne.datasets import sample
-
+from pyriemann_qiskit.classification import \
+    QuantumClassifierWithDefaultRiemannianPipeline
+from pyriemann_qiskit.utils.filtering import NaivePair
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-
-
 from sklearn.decomposition import PCA
 from matplotlib import pyplot as plt
-from sklearn.model_selection import StratifiedKFold, GridSearchCV, cross_val_predict
+from sklearn.model_selection import StratifiedKFold, GridSearchCV, \
+    cross_val_predict
 from sklearn.metrics import balanced_accuracy_score
-
 
 print(__doc__)
 
 X, y = get_mne_sample()
-
-
-###############################################################################
-# Decoding in tangent space with a quantum classifier
-
-
-
-# https://stackoverflow.com/questions/61825227/plotting-multiple-confusion-matrix-side-by-side
-
-# filters = [Closest(ndim=5), Preclassif(ndim=5), NaivePair(), NaiveImpair(), PCA(n_components=5), NoFilter()]
-
 
 nfilters = [1, 1]
 
@@ -59,22 +40,22 @@ default = {
 }
 
 pipe = QuantumClassifierWithDefaultRiemannianPipeline()
-idx = -1
 
-def get_grid_search(title, a_gamma, a_spsa_trials=[None], a_two_local_reps=[None], a_shots=default["shots"]):
+
+def get_grid_search(idx, title, a_gamma, a_spsa_trials=[None],
+                    a_two_local_reps=[None], a_shots=default["shots"]):
     params = {
-        "nfilter" : [1],
-        "gamma" : a_gamma,
+        "nfilter": [1],
+        "gamma": a_gamma,
         "dim_red": default["dim_reds"],
-        "shots" : a_shots,
-        "feature_entanglement" : default["feature_entanglement"],
-        "feature_reps" : default["reps"],
-        "spsa_trials" : a_spsa_trials,
-        "two_local_reps" : a_two_local_reps
+        "shots": a_shots,
+        "feature_entanglement": default["feature_entanglement"],
+        "feature_reps": default["reps"],
+        "spsa_trials": a_spsa_trials,
+        "two_local_reps": a_two_local_reps
     }
     grid = GridSearchCV(pipe, params, n_jobs=3)
     search = grid.fit(X, y)
-    idx += 1
     return {
         "idx": idx,
         "title": title,
@@ -83,21 +64,21 @@ def get_grid_search(title, a_gamma, a_spsa_trials=[None], a_two_local_reps=[None
     }
 
 
-QSVC = get_grid_search("QSVC", default["gamma"])
+QSVC = get_grid_search(0, "QSVC", default["gamma"])
 
-VQC = get_grid_search("VQC", [None], [40], default["reps"])
+VQC = get_grid_search(1, "VQC", [None], [40], default["reps"])
 
-SVC = get_grid_search("SVC", default["gamma"], [None], [None], [None])
+SVC = get_grid_search(2, "SVC", default["gamma"], [None], [None], [None])
 
 # Results will be computed for QuanticSVM versus SKLearnSVM for comparison
 for classif in [SVC, QSVC, VQC]:
     for i in range(len(nfilters)):
         cv = StratifiedKFold(n_splits=5, shuffle=False)
-        
+
         params = classif["best_params"]
         title = classif["title"]
-        idx = classif["idx"]
-        axe = axes[idx][i]
+        n = classif["idx"]
+        axe = axes[n][i]
 
         pipe = QuantumClassifierWithDefaultRiemannianPipeline(**params)
 
@@ -109,7 +90,8 @@ for classif in [SVC, QSVC, VQC]:
 
         names = ["vis left", "vis right"]
 
-        title = title + " (" + score_str + ")" + " (nfilter = " + str(nfilters[i]) + ")"
+        title = title + " (" + score_str + ")" + \
+            " (nfilter = " + str(nfilters[i]) + ")"
         cm = confusion_matrix(y_pred, y)
         disp = ConfusionMatrixDisplay(cm, display_labels=names)
         disp.plot(ax=axe, xticks_rotation=45)
