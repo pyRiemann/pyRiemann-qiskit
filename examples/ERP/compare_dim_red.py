6e6554efd383dcc2fdfba01b320d4b426b6bb511
gcattan <gregoire.cattan@ibm.com>
2022-03-30 11:36:38 +0200

    working with small sample

--- examples/ERP/compare_dim_red.py
+++ examples/ERP/compare_dim_red.py
@@ -10,6 +10,7 @@ TODO
 # Modified from plot_classify_EEG_tangentspace.py
 # License: BSD (3-clause)
 
+from typing import Dict
 import numpy as np
 from pyriemann_qiskit.datasets import get_mne_sample
 from pyriemann_qiskit.classification import \
@@ -27,10 +28,7 @@ print(__doc__)
 X, y = get_mne_sample(n_trials=10)
 
 nfilters = [1, 2]
-
-f, axes = plt.subplots(len(nfilters), 3, sharey='row')
-
-disp = None
+len_nfilters = len(nfilters)
 
 default = {
     "gamma": [0.01],
@@ -42,6 +40,11 @@ default = {
 
 pipe = QuantumClassifierWithDefaultRiemannianPipeline()
 
+def print_score(data, nfilter):
+    title = data["title"]
+    scores = data["best_score"]
+    print("Best %s parameter (nfilter=%i, score=%0.3f):"
+            % (title, nfilter + 1, scores[nfilter]), data["best_params"][nfilter])
 
 def get_grid_search(idx, title, a_gamma, a_spsa_trials=[None],
                     a_two_local_reps=[None], a_shots=default["shots"]):
@@ -65,12 +68,7 @@ def get_grid_search(idx, title, a_gamma, a_spsa_trials=[None],
     i_best_score_2 = np.where(scores == max(scores[filters == 2]))
     i_best_score_1 = i_best_score_1[0][0]
     i_best_score_2 = i_best_score_2[0][0]
-    # print(search.cv_results_["rank_test_score"])
-    # print(filters)
-    # print(scores)
-    # print(scores[filters == 1])
-    # print(max(scores[filters == 1]))
-    print("i_best_score_1", i_best_score_1, i_best_score_1)
+
     return {
         "idx": idx,
         "title": title,
@@ -81,56 +79,24 @@ def get_grid_search(idx, title, a_gamma, a_spsa_trials=[None],
 
 SVC = get_grid_search(2, "SVC", default["gamma"], [None], [None], [None])
 
-# print(SVC)
-# exit(0)
 QSVC = get_grid_search(0, "QSVC", default["gamma"])
 
 VQC = get_grid_search(1, "VQC", [None], [40], default["reps"])
 
-FILTER_1 = 0
-FILTER_2 = 1
+_, axes = plt.subplots(len_nfilters, 1, figsize=(10,7))
 
-
-# Results will be computed for QuanticSVM versus SKLearnSVM for comparison
-for classif in [SVC, QSVC, VQC]:
-    for i in range(len(nfilters)):
+for i in range(len_nfilters):
+    scores = [0] * 3
+    titles = [""] * 3
+    axe = axes[i]
+    for classif in [SVC, QSVC, VQC]: 
         params = classif["best_params"][i]
-        title = classif["title"]
         n = classif["idx"]
-        axe = axes[i][n]
+        titles[n] = classif["title"]
+        scores[n] = classif["best_score"][i]
+        print_score(classif, i)
+    axe.bar(titles, scores)
+    axe.set_title("Balanced accuracies (nfilter=%i)" % (i + 1))
+    
 
-
-        score = classif["best_score"][i]
-        print(classif)
-        # Printing the results
-        score_str = "%0.2f" % score
-
-        names = ["vis left", "vis right"]
-
-        title = title + " (" + score_str + ")" + \
-            " (nfilter = " + str(nfilters[i]) + ")"
-        cm = confusion_matrix(y, y)
-        disp = ConfusionMatrixDisplay(cm, display_labels=names)
-        disp.plot(ax=axe, xticks_rotation=45)
-        disp.ax_.set_title(title)
-        disp.im_.colorbar.remove()
-        disp.ax_.set_xlabel('')
-        if n > 0:
-            disp.ax_.set_ylabel('')
-        if i < 2:
-            disp.ax_.set_xlabel('')
-        # if not quantum:
-        #     disp.ax_.set_xlabel('')
-
-print("Best QSVC parameter (CV score=%0.3f):" % QSVC["best_score"][0])
-print(QSVC["best_params"])
-print("Best VQC parameter (CV score=%0.3f):" % VQC["best_score"][0])
-print(VQC["best_params"])
-print("Best SVC parameter Classical (CV score=%0.3f):" % SVC["best_score"][0])
-print(SVC["best_params"])
-
-if disp:
-    f.text(0.4, 0.1, 'Predicted label', ha='left')
-    plt.subplots_adjust(wspace=0.40, hspace=0.1)
-    f.colorbar(disp.im_, ax=axes)
-    plt.show()
+plt.show()
