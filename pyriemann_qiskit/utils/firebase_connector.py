d4d6d3979dfe2c465ec21622eebc7f67288c0cdc
gcattan <gregoire.cattan@ibm.com>
2022-10-18 17:13:05 +0200

    replace ast by eval

--- pyriemann_qiskit/utils/firebase_connector.py
+++ pyriemann_qiskit/utils/firebase_connector.py
@@ -1,6 +1,6 @@
 import firebase_admin
 import os
-import ast
+# import ast
 from firebase_admin import credentials, firestore
 from .firebase_cert import certificate
 
@@ -22,7 +22,7 @@ class FirebaseConnector():
             cred = credentials.Certificate(certificate)
         except Exception:
             env_certificate = \
-                ast.literal_eval(os.environ["FIREBASE_CERTIFICATE"])
+                eval(os.environ["FIREBASE_CERTIFICATE"])
             cred = credentials.Certificate(env_certificate)
         cred = credentials.Certificate(certificate)
         firebase_admin.initialize_app(cred)
