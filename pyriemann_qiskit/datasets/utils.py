e3d4bda1eaeb9a4422203b8b48473b99efd1482b
gcattan <gcattan@hotmail.fr>
2022-10-26 11:05:36 +0200

    Cache results for subject/pipeline (#82)
    * add dependency to firebase admin
    
    * add firebase connector
    
    * mock connector
    
    * add empty certificate for github
    
    * - retrieve certificate from environment
    - add firebase connector to init
    
    * add entry into api.rst for FirebaseConnector
    
    * add test for firebase connector
    
    * Test real connection
    
    * flake8
    
    * flake8 tests
    
    * flake8
    
    * fix relative import
    
    * fix environ not callable
    
    * update workflow with gitsecret
    
    * use ast to convert string certificate to dictionary
    
    * replace ast by eval
    
    * add double eval to escape string
    
    * revert previous commit
    
    * Change Exception -> ValueError
    
    * remove duplicated line
    
    * add documentation
    
    * flake8
    
    * add Cache abstraction
    
    * flake8
    
    * optional import for mne and firebase_admin
    correct bug with get_data
    
    * clean test
    
    * Move MockDataset to datasets/utils.
    Add some documentation
    
    * Finish documentation
    
    * small typos
    
    * Update pyriemann_qiskit/datasets/utils.py
    
    Co-authored-by: Quentin Barthélemy <q.barthelemy@gmail.com>
    
    * Update pyriemann_qiskit/utils/firebase_connector.py
    
    Co-authored-by: Quentin Barthélemy <q.barthelemy@gmail.com>
    
    * Update pyriemann_qiskit/utils/firebase_connector.py
    
    Co-authored-by: Quentin Barthélemy <q.barthelemy@gmail.com>
    
    * Update test_utils_firebase.py
    
    test_firebase_connection
    
    * Update test_utils_firebase.py
    
    Co-authored-by: gcattan <gregoire.cattan@ibm.com>
    Co-authored-by: Quentin Barthélemy <q.barthelemy@gmail.com>

--- pyriemann_qiskit/datasets/utils.py
+++ pyriemann_qiskit/datasets/utils.py
@@ -1,6 +1,10 @@
+from warnings import warn
 import numpy as np
-from mne import io, read_events, pick_types, Epochs
-from mne.datasets import sample
+try:
+    from mne import io, read_events, pick_types, Epochs
+    from mne.datasets import sample
+except Exception:
+    warn("mne not available. get_mne_sample will fail.")
 from qiskit_machine_learning.datasets import ad_hoc_data
 from sklearn.datasets import make_classification
 
@@ -33,6 +37,10 @@ def get_mne_sample(n_trials=10):
         ndarray of trials.
     y : ndarray, shape (n_trials,)
         Predicted target vector relative to X.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.1
 
     References
     ----------
@@ -81,6 +89,10 @@ def get_mne_sample(n_trials=10):
 def get_qiskit_dataset():
     """Return qiskit dataset.
 
+    Notes
+    -----
+    .. versionadded:: 0.0.1
+
     Returns
     -------
     X : ndarray, shape (n_samples, n_features)
@@ -107,6 +119,10 @@ def get_qiskit_dataset():
 
 def get_linearly_separable_dataset():
     """Return a linearly separable dataset.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.1
 
     Returns
     -------
@@ -151,6 +167,10 @@ def get_feature_dimension(dataset):
         n_features : int
             The feature dimension, -1 denotes no data in the dataset.
 
+    Notes
+    -----
+    .. versionadded:: 0.0.2
+
     Raises
     -------
     TypeError
@@ -166,3 +186,70 @@ def get_feature_dimension(dataset):
         return v.shape[1]
 
     return -1
+
+
+class MockDataset():
+    """A dataset with mock data.
+
+    Parameters
+    ----------
+    dataset_gen : Callable[[], (List, List)]
+        A function to generate datasets.
+        The function accepts no parameters and returns a pair of lists.
+        The first list contains the samples, the second one the labels.
+    n_subjects : int
+        The number of subjects in the dataset.
+        A dataset will be generated for all subjects using the handler
+        `dataset_gen`.
+
+    Attributes
+    ----------
+    code_ : str
+        The code of the dataset.
+        The code of the dataset is also the string representation
+        of the dataset.
+    data_ : Dict
+        A dictionnary representing the dataset. Ex:
+        ```
+        {
+            subject1: (samples1, labels1),
+            subject2: (samples2, labels2),
+            ...
+        }
+        ```
+    subjects_ : List[int]
+        The subjects of the dataset.
+
+    Notes
+    -----
+    .. versionadded:: 0.0.3
+
+    """
+    def __init__(self, dataset_gen, n_subjects: int):
+        self.code_ = "MockDataset"
+        self.subjects_ = range(n_subjects)
+        self.data_ = {}
+        for subject in self.subjects_:
+            self.data_[subject] = dataset_gen()
+
+    def get_data(self, subject):
+        """
+        Returns the data of a subject.
+
+        Parameters
+        ----------
+        subject : int
+            A subject in the list of subjects `subjects_`.
+
+        Returns
+        -------
+        data : the subject's data.
+
+        Notes
+        -----
+        .. versionadded:: 0.0.3
+        """
+        return self.data_[subject]
+
+    def __str__(self) -> str:
+        return self.code_
