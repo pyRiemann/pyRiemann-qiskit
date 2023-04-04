6dd10f3f04d4a15dad3727b0878497318444ac4a
gcattan <gcattan@hotmail.fr>
2023-04-04 20:26:49 +0200

    Update __init__.py

--- pyriemann_qiskit/utils/__init__.py
+++ pyriemann_qiskit/utils/__init__.py
@@ -5,7 +5,14 @@ from .docplex import (square_cont_mat_var,
                       square_bin_mat_var,
                       ClassicalOptimizer,
                       NaiveQAOAOptimizer)
-from .firebase_connector import FirebaseConnector, Cache
+from .firebase_connector import (
+        FirebaseConnector,
+        Cache,
+        generate_caches,
+        filter_subjects_with_all_results,
+        add_moabb_dataframe_results_to_caches,
+        convert_caches_to_dataframes
+    )
 
 __all__ = [
     'hyper_params_factory',
@@ -17,5 +24,9 @@ __all__ = [
     'ClassicalOptimizer',
     'NaiveQAOAOptimizer',
     'FirebaseConnector',
-    'Cache'
+    'Cache',
+    'generate_caches',
+    'filter_subjects_with_all_results',
+    'add_moabb_dataframe_results_to_caches',
+    'convert_caches_to_dataframes'
 ]
