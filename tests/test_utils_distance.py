fbb3be79f289ac1bd32f5477d985527e9b179707
gcattan <gcattan@hotmail.fr>
2023-05-23 09:30:00 +0200

    Update test_utils_distance.py
    suppress performance test for the moment

--- tests/test_utils_distance.py
+++ tests/test_utils_distance.py
@@ -9,22 +9,6 @@ from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 
-def test_performance(get_covmats, get_labels):
-    metric = {
-        'mean': "logeuclid",
-        'distance': "convex"
-    }
-
-    clf = make_pipeline(XdawnCovariances(), MDM(metric=metric))
-    skf = StratifiedKFold(n_splits=5)
-    n_matrices, n_channels, n_classes = 100, 3, 2
-    covset = get_covmats(n_matrices, n_channels)
-    labels = get_labels(n_matrices, n_classes)
-
-    score = cross_val_score(clf, covset, labels, cv=skf, scoring='roc_auc')
-    assert score.mean() > 0
-
-
 @pytest.mark.parametrize('optimizer',
                          [ClassicalOptimizer(),
                           NaiveQAOAOptimizer()])
