782d5b48916ef49fdd5e2446066f83c12ba3e845
toncho11 <toncho11@gmail.com>
2023-06-15 14:22:37 +0200

    small improvement

--- examples/ERP/classify_P300_bi_quantum_mdm_parameter_search.py
+++ examples/ERP/classify_P300_bi_quantum_mdm_parameter_search.py
@@ -93,12 +93,14 @@ paradigm = P300()
 # bi2015a        32  43
 # bi2015b        32  44
 
-datasets = [BNCI2014009()]
+datasets = [bi2013a()]
 
 # reduce the number of subjects, the Quantum pipeline takes a lot of time
 # if executed on the entire dataset
-n_subjects = 2
+n_subjects = 3
+title = ''
 for dataset in datasets:
+    title = title + dataset.code
     dataset.subject_list = dataset.subject_list[0:n_subjects]
 
 
@@ -167,7 +169,7 @@ sns.stripplot(
     zorder=1,
     palette="Set1",
 )
-sns.pointplot(data=results, y="score", x="pipeline", ax=ax, palette="Set1")
+sns.pointplot(data=results, y="score", x="pipeline", ax=ax, palette="Set1").set(title=title)
 
 ax.set_ylabel("ROC AUC")
 ax.set_ylim(0.3, 1)
