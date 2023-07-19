dffb43913c9e9fb435187881948966c369c9a207
gcattan <gcattan@hotmail.fr>
2023-07-19 09:16:35 +0200

    Update tutorial_quantum_svm_titanic_data.py
    remove deadcode

--- examples/tutorial_quantum_svm_titanic_data.py
+++ examples/tutorial_quantum_svm_titanic_data.py
@@ -13,8 +13,8 @@ https://www.kaggle.com/c/titanic/data
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from matplotlib.collections import LineCollection
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.decomposition import PCA
@@ -25,6 +25,52 @@ from sklearn.metrics import balanced_accuracy_score
 from pyriemann_qiskit.classification import QuanticSVM
 
 print(__doc__)
+
+
+###############################################################################
+# Useful functions
+
+def correlation_ratio(categories, measurements):
+    fcat, _ = pd.factorize(categories)
+    cat_num = np.max(fcat) + 1
+    y_avg_array = np.zeros(cat_num)
+    n_array = np.zeros(cat_num)
+    for i in range(0, cat_num):
+        cat_measures = measurements[np.argwhere(fcat == i).flatten()]
+        n_array[i] = len(cat_measures)
+        y_avg_array[i] = np.average(cat_measures)
+    y_total_avg = np.sum(np.multiply(y_avg_array, n_array)) / np.sum(n_array)
+    numerator = np.sum(
+        np.multiply(n_array, np.power(np.subtract(y_avg_array, y_total_avg), 2))
+    )
+    denominator = np.sum(np.power(np.subtract(measurements, y_total_avg), 2))
+    if numerator == 0:
+        eta = 0.0
+    else:
+        eta = numerator / denominator
+    return eta
+
+
+def clean(data):
+    # dropping the columns that are not significant for survival
+    droppable_columns = ["Name", "Ticket", "Cabin"]
+    data = data.drop(droppable_columns, axis=1)
+
+    # "Age" missing values replaced by the mean age
+    data["Age"].fillna(data["Age"].mean(), inplace=True)
+    # data['Age'] = data[['Age', "Pclass"]].apply(age_engineering, axis = 1)
+
+    # "Embarked" missing values replaced by U(unknown)
+    data["Embarked"].fillna("U", inplace=True)
+
+    # "SibSp", "Parch" missing values replaced by 0
+    data["SibSp"].fillna(0, inplace=True)
+    data["Parch"].fillna(0, inplace=True)
+
+    # "Fare" missing values replaced by its median
+    data["Fare"].fillna(data["Fare"].mean(), inplace=True)
+
+    return data
 
 ###############################################################################
 # Download data
@@ -82,19 +128,19 @@ fill_rate = train.notnull().mean()
 print(fill_rate)
 
 print(train["Pclass"].describe())
-sns.displot(x="Pclass", data=train_data)
+sns.displot(x="Pclass", data=train)
 
 print(train["Age"].describe())
-sns.displot(x="Age", data=train_data)
+sns.displot(x="Age", data=train)
 
 print(train["Sex"].describe())
-sns.displot(x="Sex", data=train_data)
+sns.displot(x="Sex", data=train)
 
 print(train["Embarked"].describe())
-sns.displot(x="Embarked", data=train_data)
+sns.displot(x="Embarked", data=train)
 
 print(train["Fare"].describe())
-sns.displot(x="Fare", data=train_data)
+sns.displot(x="Fare", data=train)
 
 sns.catplot(
     data=train,
@@ -226,197 +272,6 @@ print(R2)
 ###############################################################################
 # Multivariate analysis
 
-
-def correlation_ratio(categories, measurements):
-    fcat, _ = pd.factorize(categories)
-    cat_num = np.max(fcat) + 1
-    y_avg_array = np.zeros(cat_num)
-    n_array = np.zeros(cat_num)
-    for i in range(0, cat_num):
-        cat_measures = measurements[np.argwhere(fcat == i).flatten()]
-        n_array[i] = len(cat_measures)
-        y_avg_array[i] = np.average(cat_measures)
-    y_total_avg = np.sum(np.multiply(y_avg_array, n_array)) / np.sum(n_array)
-    numerator = np.sum(
-        np.multiply(n_array, np.power(np.subtract(y_avg_array, y_total_avg), 2))
-    )
-    denominator = np.sum(np.power(np.subtract(measurements, y_total_avg), 2))
-    if numerator == 0:
-        eta = 0.0
-    else:
-        eta = numerator / denominator
-    return eta
-
-
-def display_circles(
-    pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None
-):
-    for (
-        d1,
-        d2,
-    ) in axis_ranks:  # Display the 6 first components
-        if d2 < n_comp:
-            fig, ax = plt.subplots(figsize=(7, 6))
-
-            if lims is not None:
-                xmin, xmax, ymin, ymax = lims
-            elif pcs.shape[1] < 30:
-                xmin, xmax, ymin, ymax = -1, 1, -1, 1
-            else:
-                xmin, xmax, ymin, ymax = (
-                    min(pcs[d1, :]),
-                    max(pcs[d1, :]),
-                    min(pcs[d2, :]),
-                    max(pcs[d2, :]),
-                )
-
-            # display arrows
-            # only display line (without triangle at the end)
-            # if more than 30 arrows
-            if pcs.shape[1] < 30:
-                plt.quiver(
-                    np.zeros(pcs.shape[1]),
-                    np.zeros(pcs.shape[1]),
-                    pcs[d1, :],
-                    pcs[d2, :],
-                    angles="xy",
-                    scale_units="xy",
-                    scale=1,
-                    color="grey",
-                )
-            else:
-                lines = [[[0, 0], [x, y]] for x, y in pcs[[d1, d2]].T]
-                ax.add_collection(
-                    LineCollection(lines, axes=ax, alpha=0.1, color="black")
-                )
-
-            # display variable names
-            if labels is not None:
-                for i, (x, y) in enumerate(pcs[[d1, d2]].T):
-                    if x >= xmin and x <= xmax and y >= ymin and y <= ymax:
-                        plt.text(
-                            x,
-                            y,
-                            labels[i],
-                            fontsize="14",
-                            ha="center",
-                            va="center",
-                            rotation=label_rotation,
-                            color="blue",
-                            alpha=0.5,
-                        )
-
-            # display circle
-            circle = plt.Circle((0, 0), 1, facecolor="none", edgecolor="b")
-            plt.gca().add_artist(circle)
-
-            plt.xlim(xmin, xmax)
-            plt.ylim(ymin, ymax)
-
-            plt.plot([-1, 1], [0, 0], color="grey", ls="--")
-            plt.plot([0, 0], [-1, 1], color="grey", ls="--")
-
-            # Display axis with explained variance (%)
-            plt.xlabel(
-                "F{} ({}%)".format(
-                    d1 + 1, round(100 * pca.explained_variance_ratio_[d1], 1)
-                )
-            )
-            plt.ylabel(
-                "F{} ({}%)".format(
-                    d2 + 1, round(100 * pca.explained_variance_ratio_[d2], 1)
-                )
-            )
-
-            plt.title("Cercle des corrélations (F{} et F{})".format(d1 + 1, d2 + 1))
-            plt.show(block=False)
-
-
-def display_factorial_planes(
-    X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, illustrative_var=None
-):
-    for d1, d2 in axis_ranks:
-        if d2 < n_comp:
-            plt.figure(figsize=(7, 6))
-
-            # print points
-            if illustrative_var is None:
-                plt.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha)
-            else:
-                illustrative_var = np.array(illustrative_var)
-                for value in np.unique(illustrative_var):
-                    selected = np.where(illustrative_var == value)
-                    plt.scatter(
-                        X_projected[selected, d1],
-                        X_projected[selected, d2],
-                        alpha=alpha,
-                        label=value,
-                    )
-                plt.legend()
-
-            # print labels
-            if labels is not None:
-                for i, (x, y) in enumerate(X_projected[:, [d1, d2]]):
-                    plt.text(x, y, labels[i], fontsize="14", ha="center", va="center")
-
-            # set graphic frame
-            boundary = np.max(np.abs(X_projected[:, [d1, d2]])) * 1.1
-            plt.xlim([-boundary, boundary])
-            plt.ylim([-boundary, boundary])
-
-            plt.plot([-100, 100], [0, 0], color="grey", ls="--")
-            plt.plot([0, 0], [-100, 100], color="grey", ls="--")
-
-            # print explained variance (%)
-            plt.xlabel(
-                "F{} ({}%)".format(
-                    d1 + 1, round(100 * pca.explained_variance_ratio_[d1], 1)
-                )
-            )
-            plt.ylabel(
-                "F{} ({}%)".format(
-                    d2 + 1, round(100 * pca.explained_variance_ratio_[d2], 1)
-                )
-            )
-
-            plt.title(
-                "Projection des individus (sur F{} et F{})".format(d1 + 1, d2 + 1)
-            )
-            plt.show(block=False)
-
-
-def display_scree_plot(pca):
-    scree = pca.explained_variance_ratio_ * 100
-    plt.bar(np.arange(len(scree)) + 1, scree)
-    plt.plot(np.arange(len(scree)) + 1, scree.cumsum(), c="red", marker="o")
-    plt.xlabel("rang de l'axe d'inertie")
-    plt.ylabel("pourcentage d'inertie")
-    plt.title("Eboulis des valeurs propres")
-    plt.show(block=False)
-
-
-def clean(data):
-    # dropping the columns that are not significant for survival
-    droppable_columns = ["Name", "Ticket", "Cabin"]
-    data = data.drop(droppable_columns, axis=1)
-
-    # "Age" missing values replaced by the mean age
-    data["Age"].fillna(data["Age"].mean(), inplace=True)
-    # data['Age'] = data[['Age', "Pclass"]].apply(age_engineering, axis = 1)
-
-    # "Embarked" missing values replaced by U(unknown)
-    data["Embarked"].fillna("U", inplace=True)
-
-    # "SibSp", "Parch" missing values replaced by 0
-    data["SibSp"].fillna(0, inplace=True)
-    data["Parch"].fillna(0, inplace=True)
-
-    # "Fare" missing values replaced by its median
-    data["Fare"].fillna(data["Fare"].mean(), inplace=True)
-
-    return data
-
-
 # # ANOVA Rule of thumb
 correlation_ratio(features["Sex"], features["Survived"])
 
@@ -484,8 +339,6 @@ plt.show()
 ###############################################################################
 # Classification
 
-# # Logistic Regression
-
 # Cleaning
 train = clean(train)
 
@@ -493,10 +346,12 @@ train = clean(train)
 train = pd.get_dummies(train, columns=["Sex", "Embarked", "Pclass"])
 
 # train/test validation
+# (the same for all classifiers - avoid biases in comparison)
 X_train, X_test, y_train, y_test = train_test_split(
     train.drop("Survived", axis=1), train["Survived"], test_size=0.2, random_state=42
 )
 
+# Logistic Regression
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
