8d5f1a01a36c204878986560cf02613fbd2b1676
Gregoire Cattan <gregoire.cattan@ibm.com>
2023-09-16 14:38:43 +0200

    spiral implementation

--- examples/toys_dataset/plot_quantum_art_vqc.py
+++ examples/toys_dataset/plot_quantum_art_vqc.py
@@ -12,6 +12,7 @@ classifiers on toys datasets.
 # Modified for pyRiemann-qiskit by Gregoire Cattan
 # License: BSD 3 clause
 
+import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -27,23 +28,56 @@ from pyriemann_qiskit.classification import QuanticVQC
 # from pyriemann_qiskit.classification import QuanticVQC
 
 
+
+
+
+
 print(__doc__)
 
 ###############################################################################
 
 vqc = QuanticVQC()
 
-for i in range(10):
+weights = []
+for i in range(5):
     X, y = get_qiskit_dataset(n_trials=2)
 
     vqc.fit(X, y)
 
     train_weights = vqc._classifier.weights
+    weights.append(train_weights)
+    # print(vqc._classifier.ansatz.parameters)
+    
 
-    sns.lineplot(train_weights)
+df = pd.DataFrame(weights)
 
+theta = np.arange(0, 8*np.pi, 0.1)
+a = 1
+b = .2
 
+n_params = len(df)
+
+max_var = max([df[i].var() for i in range(n_params)])
+
+for i in range(n_params):
+
+    dt = 2*np.pi / n_params * i
+    x = a*np.cos(theta + dt)*np.exp(b*theta)
+    y = a*np.sin(theta + dt)*np.exp(b*theta)
+    # print(df.iloc[i])
+    var = df[i].var()
+    print(max_var)
+    print(var)
+    # dt = dt + np.pi/4.0
+    
+    dt = dt + (var / max_var) * np.pi/4.0
+
+    x2 = a*np.cos(theta + dt)*np.exp(b*theta)
+    y2 = a*np.sin(theta + dt)*np.exp(b*theta)
+
+    xf = np.concatenate((x, x2[::-1]))
+    yf = np.concatenate((y, y2[::-1]))
+
+    p1 = plt.fill(xf, yf)
 
 plt.show()
-
-
