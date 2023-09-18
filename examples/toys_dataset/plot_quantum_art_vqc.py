dcb3136c7dc1191ee6c93bcabc5d36083fe415b7
Gregoire Cattan <gregoire.cattan@ibm.com>
2023-09-18 11:41:36 +0200

    comment code

--- examples/toys_dataset/plot_quantum_art_vqc.py
+++ examples/toys_dataset/plot_quantum_art_vqc.py
@@ -1,16 +1,15 @@
 """
 ====================================================================
-Comparison with toys datasets.
+Art visualization of Variational Quantum Classifier.
 ====================================================================
 
-Comparison of classification using quantum versus classical SVM
-classifiers on toys datasets.
+Display the variability of the weights inside the variational quantum
+classifier.
 
 """
-# Code source:
-# https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
-# Modified for pyRiemann-qiskit by Gregoire Cattan
-# License: BSD 3 clause
+
+# Author: Gregoire Cattan
+# License: BSD (3-clause)
 
 from pyriemann_qiskit.utils.hyper_params_factory import gen_two_local
 import matplotlib.pyplot as plt
@@ -22,21 +21,39 @@ from pyriemann_qiskit.visualization import weights_spiral
 print(__doc__)
 
 ###############################################################################
+# In this example we will display weights variability of the parameter inside 
+# the variational quantum circuit which is used by VQC.
+# 
+# The idea is simple :
+# - We initialize a VQC with different number of parameters and number of samples
+# - We train the VQC a couple of time and we store the fitted weights.
+# - We compute variability of the weight and display it in a fashion way.
 
+# Let's start by defining some plot area.
 fig, axes = plt.subplots(2, 2)
-fig.suptitle('Vertically stacked subplots')
+fig.suptitle('VQC weights variability')
 
-vqc_low_param = QuanticVQC(gen_var_form=gen_two_local(reps=1))
-vqc = QuanticVQC(gen_var_form=gen_two_local(reps=2))
-
-for i, n_samples in enumerate([2, 3]):
+# We will compute weight variability for different number of samples
+for i, n_samples in enumerate([2, 5]):
+    # ... and for differente number of parameters.
+    # (n_reps controls the number of parameters inside the circuit)
     for j, n_reps in enumerate([1, 2]):
+
+        # instanciate VQC.
         vqc = QuanticVQC(gen_var_form=gen_two_local(reps=n_reps))
+
+        # Get data. We will use a toy dataset here.
         X, y = get_linearly_separable_dataset(n_samples=n_samples)
+
+        # Compute and display weight variability after training
         axe = axes[i, j]
-        
-        weights_spiral(axe, vqc, X, y)
+        # ... This is all done in this method
+        # It displays a spiral. Each "branch of the spiral" is a parameter inside VQC.
+        # The larger is the branch, the higher is the parameter variability.
+        weights_spiral(axe, vqc, X, y, n_trainings=5)
         n_params = vqc.parameter_count
+
+        # Just improve display of the graphics. 
         if j == 0:
             axe.set_ylabel(f"n_samples: {n_samples}")
         if i == 0:
