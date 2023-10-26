6f2c87c00c54bef22d0e5d14daf96d967ea9827f
Gregoire Cattan <gregoire.cattan@ibm.com>
2023-10-26 21:24:41 +0200

    new implementation of ndstandardscaler

--- examples/other_datasets/plot_financial_data.py
+++ examples/other_datasets/plot_financial_data.py
@@ -131,38 +131,22 @@ class ToEpochs(TransformerMixin, BaseEstimator):
 
 # Stackoverflow implementation [4]_
 class NDStandardScaler(TransformerMixin):
-    def __init__(self, **kwargs):
-        self._scaler = StandardScaler(copy=True, **kwargs)
-        self._orig_shape = None
+    def __init__(self):
+        self._scalers = []
 
     def fit(self, X, y=None, **kwargs):
-        X = np.array(X)
-        # Save the original shape to reshape the flattened X later
-        # back to its original shape
-        if len(X.shape) > 1:
-            self._orig_shape = X.shape[1:]
-        X = self._flatten(X)
-        self._scaler.fit(X, y, **kwargs)
+        _, n_channels, _ = X.shape
+        self._scalers = []
+        for i in range(n_channels):
+            scaler = StandardScaler()
+            scaler.fit(X[:, i, :])
+            self._scalers.append(scaler)
         return self
 
     def transform(self, X, **kwargs):
-        X = np.array(X)
-        X = self._flatten(X)
-        X = self._scaler.transform(X, **kwargs)
-        X = self._reshape(X)
-        return X
-
-    def _flatten(self, X):
-        # Reshape X to <= 2 dimensions
-        if len(X.shape) > 2:
-            n_dims = np.prod(self._orig_shape)
-            X = X.reshape(-1, n_dims)
-        return X
-
-    def _reshape(self, X):
-        # Reshape X back to it's original shape
-        if len(X.shape) >= 2:
-            X = X.reshape(-1, *self._orig_shape)
+        n_channels = len(self._scalers)
+        for i in range(n_channels):
+            X[:, i, :] = self._scalers[i].transform(X[:, i, :])
         return X
 
 
