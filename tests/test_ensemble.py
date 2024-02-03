a7bf4d1e7b21ac110fbbdfc54dd9e1b0c565194c
gcattan <gcattan@hotmail.fr>
2024-02-03 18:32:25 +0100

    push tests

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -1,7 +1,47 @@
 from pyriemann_qiskit.ensemble import (
     JudgeClassifier,
 )
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.base import ClassifierMixin
+import numpy as np
 
+def test_canary():
+    assert JudgeClassifier() is not None
 
-def test_canary(estimator):
-    assert not JudgeClassifier() is None
+def test_get_set_params():
+    X = np.array([[0], [0], [1]])
+    y = np.array([0, 0, 1])
+    estimator = JudgeClassifier(LDA(), LDA(), LDA())
+    scr = cross_val_score(
+        estimator, X, y, cv=skf, scoring="roc_auc", error_score="raise"
+    )
+    assert scr.mean() > 0
+
+def test_predict():
+    X = np.array([[0], [0], [1]])
+    y = np.array([0, 0, 1])
+    class C1(ClassifierMixin):
+        def fit(self, _X, _y):
+            return self
+        def predict(self, _X):
+            return np.array([0, 1, 1])
+        
+    class C2(ClassifierMixin):
+        def fit(self, _X, _y):
+            return self
+        def predict(self, _X):
+            return np.array([0, 0, 0]) 
+
+    class Judge(ClassifierMixin):
+        def fit(self, _X, _y):
+            return self
+        def predict(self, _X):
+            # C1 and C2 will disagree on second and third prediction
+            # return the true label of the second and third prediction
+            return [0, 1]
+    
+
+    estimator = JudgeClassifier(C1(), C2(), Judge())
+    estimator.fit(X, y)
+    y_pred = estimator.predict(X)
+    assert y_pred == y
