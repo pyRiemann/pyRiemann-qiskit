4b64106bef635c3adc6f5a6091cdadb8b68c48e7
Gregoire Cattan <gregoire.cattan@ibm.com>
2024-02-08 09:54:16 +0100

    add tests with 3 classifiers

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -24,24 +24,22 @@ def test_get_set_params():
     )
     assert scr.mean() > 0
 
+class FixedPredClassifier(ClassifierMixin):
+    def __init__(self, y_pred) -> None:
+        self.y_pred = y_pred
+
+    def fit(self, _X, _y):
+        return self
+
+    def predict(self, _X):
+        return np.array(self.y_pred)
 
 def test_judge_required():
     X = np.array([[0], [0], [1]])
     y = np.array([0, 0, 1])
 
-    class C1(ClassifierMixin):
-        def fit(self, _X, _y):
-            return self
-
-        def predict(self, _X):
-            return np.array([0, 1, 1])
-
-    class C2(ClassifierMixin):
-        def fit(self, _X, _y):
-            return self
-
-        def predict(self, _X):
-            return np.array([0, 0, 0])
+    c1 = FixedPredClassifier([0, 1, 1])
+    c2 = FixedPredClassifier([0, 0, 0])
 
     class Judge(ClassifierMixin):
         def fit(self, X, y):
@@ -51,11 +49,37 @@ def test_judge_required():
             return self
 
         def predict(self, _X):
-            # C1 and C2 will disagree on second and third predictions
+            # c1 and c2 will disagree on second and third predictions
             # return the true label of the second and third predictions
             return np.array([0, 1])
 
-    estimator = JudgeClassifier(Judge(), clfs=[C1(), C2()])
+    estimator = JudgeClassifier(Judge(), clfs=[c1, c2])
+    estimator.fit(X, y)
+    y_pred = estimator.predict(X)
+    assert np.array_equal(y, y_pred)
+
+
+def test_judge_required_3_classifiers():
+    X = np.array([[0], [0], [1], [1]])
+    y = np.array([0, 0, 1, 1])
+
+    c1 = FixedPredClassifier([0, 1, 1, 1])
+    c2 = FixedPredClassifier([0, 0, 0, 1])
+    c3 = FixedPredClassifier([0, 1, 1, 0])
+
+    class Judge(ClassifierMixin):
+        def fit(self, X, y):
+            # Check that the tree last samples were identified as different
+            assert np.array_equal(X, [[0], [1], [1]])
+            assert np.array_equal(y, [0, 1, 1])
+            return self
+
+        def predict(self, _X):
+            # c1, c2 and c3 will disagree on 2nd, 3rd and 4th predictions
+            # return the true label of these predictions.
+            return np.array([0, 1, 1])
+
+    estimator = JudgeClassifier(Judge(), clfs=[c1, c2, c3])
     estimator.fit(X, y)
     y_pred = estimator.predict(X)
     assert np.array_equal(y, y_pred)
@@ -65,19 +89,7 @@ def test_judge_not_required():
     X = np.array([[0], [0], [1]])
     y = np.array([0, 0, 1])
 
-    class C1(ClassifierMixin):
-        def fit(self, _X, _y):
-            return self
-
-        def predict(self, _X):
-            return np.array([0, 0, 1])
-
-    class C2(ClassifierMixin):
-        def fit(self, _X, _y):
-            return self
-
-        def predict(self, _X):
-            return np.array([0, 0, 1])
+    clf = FixedPredClassifier([0, 0, 1])
 
     class Judge(ClassifierMixin):
         def fit(self, X_judge, y_judge):
@@ -90,6 +102,12 @@ def test_judge_not_required():
             # Check the judge is never called on predict as the two classifiers agree
             assert False
 
-    estimator = JudgeClassifier(Judge(), clfs=[C1(), C2()])
+    ## Test with two same classifiers
+    estimator = JudgeClassifier(Judge(), clfs=[clf, clf])
+    estimator.fit(X, y)
+    estimator.predict(X)
+
+    ## ... and three
+    estimator = JudgeClassifier(Judge(), clfs=[clf, clf, clf])
     estimator.fit(X, y)
     estimator.predict(X)
