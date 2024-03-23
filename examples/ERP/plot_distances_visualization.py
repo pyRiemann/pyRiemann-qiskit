bac51f341063525186f5aaa83046cd8eaa1c522b
Gregoire Cattan <gregoire.cattan@ibm.com>
2024-03-24 00:12:53 +0100

    run example on Ci

--- examples/ERP/plot_distances_visualization.py
+++ examples/ERP/plot_distances_visualization.py
@@ -1,39 +1,32 @@
 """
 ====================================================================
-Classification of P300 datasets from MOABB using NCH
+Visualize distances with MDM and NCH
 ====================================================================
 
-Demonstrates classification with QunatumNCH.
-Evaluation is done using MOABB.
-
-If parameter "shots" is None then a classical SVM is used similar to the one
-in scikit learn.
-If "shots" is not None and IBM Qunatum token is provided with "q_account_token"
-then a real Quantum computer will be used.
-You also need to adjust the "n_components" in the PCA procedure to the number
-of qubits supported by the real quantum computer you are going to use.
-A list of real quantum  computers is available in your IBM quantum account.
+Demonstrates how to use the visualization module,
+to plot the distances between the classes.
 
 """
-# Author: Anton Andreev
-# Modified from plot_classify_EEG_tangentspace.py of pyRiemann
+# Author: Gregoire Cattan
 # License: BSD (3-clause)
 
 import warnings
 
 from matplotlib import pyplot as plt
 from moabb import set_log_level
-from moabb.datasets import bi2013a, bi2012, Cattan2019_PHMD
-from moabb.evaluations import WithinSessionEvaluation
-from moabb.paradigms import P300, RestingStateToP300Adapter 
+from moabb.datasets import bi2012
+from moabb.paradigms import P300
 from pyriemann.classification import MDM
 from pyriemann.estimation import XdawnCovariances
-import seaborn as sns
+from pyriemann.preprocessing import Whitening
+
 from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
 
 from pyriemann_qiskit.classification import QuanticNCH
 from pyriemann_qiskit.visualization.distances import plot_bihist, plot_scatter
 from pyriemann_qiskit.visualization.manifold import plot_manifold
+
 
 print(__doc__)
 
@@ -50,12 +43,15 @@ set_log_level("info")
 # Create Pipelines
 # ----------------
 #
-# Pipelines must be a dict of sklearn pipeline transformer.
+# Create a MDM and NCH pipeline, as well as an estimator for 2x2 cov matrices.
 
 
 paradigm = P300(resample=128)
 
 ds = bi2012()
+
+# Change this to use NCH instead of MDM estimator for the distances
+USE_MDM = True
 
 mdm = make_pipeline(
     # applies XDawn and calculates the covariance matrix, output it matrices
@@ -64,12 +60,10 @@ mdm = make_pipeline(
         estimator="scm",
         xdawn_estimator="lwf",
     ),
-    MDM(
-    ),
+    MDM(metric="logeuclid"),
 )
 
 nch = make_pipeline(
-    # applies XDawn and calculates the covariance matrix, output it matrices
     XdawnCovariances(
         nfilter=3,
         estimator="scm",
@@ -84,26 +78,48 @@ nch = make_pipeline(
     ),
 )
 
-from pyriemann.preprocessing import Whitening
-xd = make_pipeline(XdawnCovariances(
+
+estimator = mdm if USE_MDM else nch
+
+cov2x2 = make_pipeline(
+    XdawnCovariances(
         nfilter=3,
         estimator="scm",
         xdawn_estimator="lwf",
-    ), Whitening(dim_red={"n_components": 2})
-    
-                  )
+    ),
+    Whitening(dim_red={"n_components": 2})
+)
+
+##############################################################################
+# Data
+# ----------------
+#
+# Retrieve data from bi2012
+
 
 X, y, _ = paradigm.get_data(ds, subjects=[1])
-
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
 
-dists = mdm.fit(X_train, y_train).transform(X_test)
+##############################################################################
+# Plot manifold
+# ----------------
+#
+# Plot cov matrices in 3d cartesian space. 
+# (they form a cone)
 
-points = xd.fit(X_train, y_train).transform(X_test)
+points = cov2x2.fit(X_train, y_train).transform(X_test)
 ax = plot_manifold(points, y_test, False)
 
-fig1 = plot_scatter(dists, y_test)
-fig2 = plot_bihist(dists, y_test)
+
+##############################################################################
+# Plot distances
+# ----------------
+#
+# Plot distances between the classes with the MDM or NCH estimator.
+
+dists = estimator.fit(X_train, y_train).transform(X_test)
+
+plot_scatter(dists, y_test)
+plot_bihist(dists, y_test)
 
 plt.show()
