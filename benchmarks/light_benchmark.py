6f2fc0e39a13a4b39b7ec4e49c6d27f5dd979f13
gcattan <gcattan@hotmail.fr>
2024-04-28 10:03:17 +0200

    Update light_benchmark.py (#273)
    * Update light_benchmark.py
    
    Trunc score at 10^-2 when comparing performance results between PR and main branches.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- benchmarks/light_benchmark.py
+++ benchmarks/light_benchmark.py
@@ -157,5 +157,8 @@ else:
     for key, score in scores.items():
         i = i + 1
         pr_score = sys.argv[i]
-        success = success and (True if float(pr_score) >= score else False)
+        pr_score_trun = int(float(pr_score) * 100)
+        score_trun = int(score * 100)
+        success = success and (True if pr_score_trun >= score_trun else False)
+        print(f"{key}: {pr_score_trun} (PR) >= {score} (main): {success}")
     set_output("success", "1" if success else "0")
