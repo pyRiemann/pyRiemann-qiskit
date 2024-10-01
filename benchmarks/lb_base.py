bd3e14e5106d45790b8972f92c59e4d44e5de709
Gregoire Cattan <gregoire.cattan@ibm.com>
2024-10-01 21:35:28 +0200

    factorize code in base module

---
+++ benchmarks/lb_base.py
@@ -0,0 +1,90 @@
+"""
+====================================================================
+Light Benchmark
+====================================================================
+
+Common script to run light benchmarks
+
+"""
+# Author: Gregoire Cattan
+# Modified from plot_classify_P300_bi.py of pyRiemann
+# License: BSD (3-clause)
+
+import sys
+import warnings
+
+from moabb import set_log_level
+from moabb.datasets import bi2012
+from moabb.paradigms import P300
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+
+print(__doc__)
+
+##############################################################################
+# getting rid of the warnings about the future
+warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.simplefilter(action="ignore", category=RuntimeWarning)
+
+warnings.filterwarnings("ignore")
+
+set_log_level("info")
+
+##############################################################################
+# Prepare data
+# -------------
+#
+##############################################################################
+
+def _set_output(key: str, value: str):
+        print(f"::set-output name={key}::{value}")  # noqa: E231
+
+def run(pipelines):
+    paradigm = P300(resample=128)
+
+    dataset = bi2012()  # MOABB provides several other P300 datasets
+
+    X, y, _ = paradigm.get_data(dataset, subjects=[1])
+
+    # Reduce the dataset size for Ci
+    _, X, _, y = train_test_split(X, y, test_size=0.7, random_state=42, stratify=y)
+
+    y = LabelEncoder().fit_transform(y)
+
+    # Separate into train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42, stratify=y
+    )
+
+    # Compute scores
+    scores = {}
+
+    for key, pipeline in pipelines.items():
+        pipeline.fit(X_train, y_train)
+        y_pred = pipeline.predict(X_test)
+        score = balanced_accuracy_score(y_test, y_pred)
+        scores[key] = score
+
+    print("Scores: ", scores)
+
+    # Compare scores between PR and main branches
+    is_pr = sys.argv[1] == "pr"
+
+    if is_pr:
+        for key, score in scores.items():
+            _set_output(key, score)
+    else:
+        success = True
+        i = 0
+        for key, score in scores.items():
+            i = i + 1
+            pr_score = sys.argv[i]
+            pr_score_trun = int(float(pr_score) * 100)
+            score_trun = int(score * 100)
+            better_pr_score = pr_score_trun >= score_trun
+            success = success and better_pr_score
+            print(f"{key}: {pr_score_trun} (PR) >= {score_trun} (main): {better_pr_score}")
+        _set_output("success", "1" if success else "0")
--- benchmarks/light_benchmark.py
+++ benchmarks/light_benchmark.py
@@ -11,12 +11,9 @@ to run on Ci with each PRs.
 # Modified from plot_classify_P300_bi.py of pyRiemann
 # License: BSD (3-clause)
 
-import sys
 import warnings
 
 from moabb import set_log_level
-from moabb.datasets import bi2012
-from moabb.paradigms import P300
 from pyriemann.estimation import XdawnCovariances, Shrinkage
 from pyriemann.tangentspace import TangentSpace
 from pyriemann_qiskit.pipelines import (
@@ -26,11 +23,8 @@ from pyriemann_qiskit.pipelines import (
 from pyriemann_qiskit.utils import distance, mean  # noqa
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.metrics import balanced_accuracy_score
-from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import LabelEncoder
-
+from .lb_base import run
 
 print(__doc__)
 
@@ -42,28 +36,6 @@ warnings.simplefilter(action="ignore", category=RuntimeWarning)
 warnings.filterwarnings("ignore")
 
 set_log_level("info")
-
-##############################################################################
-# Prepare data
-# -------------
-#
-##############################################################################
-
-paradigm = P300(resample=128)
-
-dataset = bi2012()  # MOABB provides several other P300 datasets
-
-X, y, _ = paradigm.get_data(dataset, subjects=[1])
-
-# Reduce the dataset size for Ci
-_, X, _, y = train_test_split(X, y, test_size=0.7, random_state=42, stratify=y)
-
-y = LabelEncoder().fit_transform(y)
-
-# Separate into train and test
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42, stratify=y
-)
 
 ##############################################################################
 # Create Pipelines
@@ -113,46 +85,9 @@ pipelines["RG_LDA"] = make_pipeline(
 )
 
 ##############################################################################
-# Compute score
-# --------------
-#
+# Compute score and compare with PR branch
+# ------------------------------------------
+# 
 ##############################################################################
 
-scores = {}
-
-for key, pipeline in pipelines.items():
-    pipeline.fit(X_train, y_train)
-    y_pred = pipeline.predict(X_test)
-    score = balanced_accuracy_score(y_test, y_pred)
-    scores[key] = score
-
-print("Scores: ", scores)
-
-##############################################################################
-# Compare scores between PR and main branches
-# -------------------------------------------
-#
-##############################################################################
-
-
-def set_output(key: str, value: str):
-    print(f"::set-output name={key}::{value}")  # noqa: E231
-
-
-is_pr = sys.argv[1] == "pr"
-
-if is_pr:
-    for key, score in scores.items():
-        set_output(key, score)
-else:
-    success = True
-    i = 0
-    for key, score in scores.items():
-        i = i + 1
-        pr_score = sys.argv[i]
-        pr_score_trun = int(float(pr_score) * 100)
-        score_trun = int(score * 100)
-        better_pr_score = pr_score_trun >= score_trun
-        success = success and better_pr_score
-        print(f"{key}: {pr_score_trun} (PR) >= {score_trun} (main): {better_pr_score}")
-    set_output("success", "1" if success else "0")
+run(pipelines)
