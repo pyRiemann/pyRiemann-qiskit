be477abf7778bc23e320413936ebe57139ca0125
gcattan <gcattan@hotmail.fr>
2024-10-28 20:26:57 +0100

    Update light_benchmark.py

--- benchmarks/light_benchmark.py
+++ benchmarks/light_benchmark.py
@@ -48,14 +48,14 @@ set_log_level("info")
 pipelines = {}
 
 pipelines["RG_QSVM"] = QuantumClassifierWithDefaultRiemannianPipeline(
-    shots=1,
+    shots=200,
     nfilter=2,
     dim_red=PCA(n_components=5),
     params={"seed": 42, "use_fidelity_state_vector_kernel": True},
 )
 
 pipelines["RG_VQC"] = QuantumClassifierWithDefaultRiemannianPipeline(
-    shots=1, spsa_trials=1, two_local_reps=2, params={"seed": 42}
+    shots=1, spsa_trials=100, two_local_reps=2, params={"seed": 42}
 )
 
 pipelines["QMDM_mean"] = QuantumMDMWithRiemannianPipeline(
