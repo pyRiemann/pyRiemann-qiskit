107281e44639d522bd9b31c5934fc1f42410155d
gcattan <gcattan@hotmail.fr>
2025-02-24 21:46:58 +0100

    Update pyriemann_qiskit/classification.py
    Co-authored-by: Quentin Barthélemy <q.barthelemy@gmail.com>

--- pyriemann_qiskit/classification.py
+++ pyriemann_qiskit/classification.py
@@ -1152,7 +1152,7 @@ class QuanticNCH(QuanticClassifierBase):
     n_reps : int, default=3
         The number of time the mixer and cost operator are repeated in the QAOA-CV
         circuit.
-    qaoa_initial_points: Tuple[int, int], default [0.0, 0.0].
+    qaoa_initial_points : Tuple[int, int], default=[0.0, 0.0].
         Starting parameters (beta and gamma) for the NaiveQAOAOptimizer.
 
     References
