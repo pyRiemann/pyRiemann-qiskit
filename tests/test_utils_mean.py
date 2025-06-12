5722bbda346732304a7229c1adf6d03af57a7950
gcattan <gcattan@hotmail.fr>
2025-06-12 20:17:51 +0200

    Update test_utils_mean.py

--- tests/test_utils_mean.py
+++ tests/test_utils_mean.py
@@ -63,8 +63,8 @@ def test_mean_cpm_shape(get_covmats, mean):
     "optimizer",
     [
         ClassicalOptimizer(optimizer=ADMMOptimizer()),
-        NaiveQAOAOptimizer(),
-        QAOACVOptimizer(),
+      #  NaiveQAOAOptimizer(),
+       # QAOACVOptimizer(),
     ],
 )
 @pytest.mark.parametrize("mean", [qmean_euclid])
@@ -81,8 +81,8 @@ def test_mean_cpm_all_zeros(optimizer, mean):
     "optimizer",
     [
         ClassicalOptimizer(optimizer=ADMMOptimizer()),
-        NaiveQAOAOptimizer(),
-        QAOACVOptimizer(),
+       # NaiveQAOAOptimizer(),
+       # QAOACVOptimizer(),
     ],
 )
 @pytest.mark.parametrize("mean", [qmean_euclid])
@@ -99,8 +99,8 @@ def test_mean_cpm_all_ones(optimizer, mean):
     "optimizer",
     [
         ClassicalOptimizer(optimizer=ADMMOptimizer()),
-        NaiveQAOAOptimizer(),
-        QAOACVOptimizer(),
+        # NaiveQAOAOptimizer(),
+        # QAOACVOptimizer(),
     ],
 )
 @pytest.mark.parametrize("mean", [qmean_euclid])
@@ -117,8 +117,8 @@ def test_mean_cpm_all_equals(optimizer, mean):
     "optimizer",
     [
         ClassicalOptimizer(optimizer=ADMMOptimizer()),
-        NaiveQAOAOptimizer(),
-        QAOACVOptimizer(),
+        # NaiveQAOAOptimizer(),
+        # QAOACVOptimizer(),
     ],
 )
 @pytest.mark.parametrize("mean", [qmean_euclid])
